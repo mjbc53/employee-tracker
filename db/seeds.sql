fb8d859c09d4c306144d9016bf96056bb7ff6fc4
Mark <mjbc53@gmail.com>
2021-11-07 17:55:25 -0600

    more finishing touches on data base foreign keys

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,7 +14,6 @@ VALUES
 ('Software develper', 80000.00, 1),
 ('Sales lead', 90000.00, 2),
 ('Sales person', 90000.00, 2),
-('Human resources', 25000.00, 5),
 ('Marking lead', 200000.00, 4),
 ('Marking helper', 200000.00, 4),
 ('Accounting lead', 150000.00, 6),
@@ -22,7 +21,8 @@ VALUES
 ('Production lead', 80000.00, 3),
 ('Production helper', 80000.00, 3),
 ('Engineering lead', 80000.00, 7),
-('Engineering helper', 80000.00, 7);
+('Engineering helper', 80000.00, 7),
+('Human resources', 25000.00, 5);
 
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
@@ -30,14 +30,16 @@ VALUES
 ('Mark', 'John', 1, NULL),
 ('Johnson', 'John', 2, 1),
 ('Jackson', 'John', 2, 1),
-('julie', 'Johnson', 4, 3),
-('james', 'jack', 3, NULL),
+('julie', 'Johnson', 3, NULL),
+('james', 'jack', 4, 3),
 ('jack', 'james', 5, NULL),
 ('jlow', 'yoyo', 6, 5),
-('Mark', 'John', 7, NULL),
-('Mark', 'John', 8, NULL),
-('Mark', 'John', 9, 8),
-('Mark', 'John', 10, NULL),
-('Mark', 'John', 11, 10);
+('jackle', 'liken', 7, NULL),
+('juju', 'joji', 8, 7),
+('jeff', 'yesuf', 9, NULL),
+('larry', 'amade', 10, 9),
+('jesse', 'doe', 11, NULL),
+('jackie', 'doe', 12, 11),
+('thomas', 'june', 13, NULL);
 
 
