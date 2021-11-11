15e3e1ba1218642627531020ae4b053e58e4ae79
Mark <mjbc53@gmail.com>
2021-11-10 23:28:48 -0600

    changed connection.js to use dotenv npm package

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2')
 
+require('dotenv').config()
+
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    user: 'root',
-    password: 'L3g0mjbc1234!',
-    database: 'company'
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
   },
   console.log('Connected to the company database')
 )
