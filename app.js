7eed9eba149c76cfd3a34a9c36fe7e168bb81415
Mark <mjbc53@gmail.com>
2021-11-13 12:24:50 -0600

    added comments to Prompts.js Query.js app.js. restructered how the prompts creates and updates and employee so that it is dry. updated schema.sql so values can be null

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//import Prompts calls
 const Prompts = require('./lib/Prompts')
 
+//call new Prompts class and call welcome method to start off the prompts
 new Prompts().welcome()
--- db/schema.sql
+++ db/schema.sql
@@ -5,22 +5,22 @@ DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  names VARCHAR(30)
+  names VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL(10,2),
-  department_id INTEGER,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER NOT NULL,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INTEGER, 
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL, 
   manager_id INTEGER DEFAULT NULL,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
