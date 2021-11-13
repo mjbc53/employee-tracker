b6675d388bae35b8f30f0cda0d15cd30746d68d4
Mark <mjbc53@gmail.com>
2021-11-12 21:35:34 -0600

    completed query calls and the rest of the code for the add employee prompts method

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE employees (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INTEGER, 
-  manager_id INTEGER,
+  manager_id INTEGER DEFAULT NULL,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
